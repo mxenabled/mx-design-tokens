934db2b7f99666b37a17f7db720c63fc807c46d4
Cameron Wengert <cameron.wengert@mx.com>
2019-11-20 17:36:39 -0500

    Add gitignore, small message change

--- build.js
+++ build.js
@@ -22,7 +22,7 @@ themeNames.forEach(themeName => {
 
   fs.writeFile(`${themeName}.json`, jsonContent, 'utf8', function (err) {
     if (err) {
-      console.log("An error occured while writing JSON Object to File.");
+      console.log(`An error occured while writing ${themeName} JSON Object to File.`)
       return console.log(err);
     }
 
