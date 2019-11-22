77ce4acd39dee064a4a2348d6f54639094f21d6a
Cameron Wengert <cameron.wengert@mx.com>
2019-11-22 13:50:46 -0500

    Got JSON working. Where should it live?

--- build.js
+++ build.js
@@ -1,19 +1,18 @@
-const themes = require('./index.js')
+const themeImport = require('./dist/index.js')
 
 const fs = require('fs');
 
-const themeNames = ['light', 'dark']
+const themes = [{ name: 'light', theme: themeImport.light}, { name: 'dark', theme: themeImport.dark}]
 
-themeNames.forEach(themeName => {
-  const jsonContent = JSON.stringify(themes[themeName])
+themes.forEach(themeObj => {
+  const jsonContent = JSON.stringify(themeObj.theme)
 
-  fs.writeFile(`./json/${themeName}.json`, jsonContent, 'utf8', function (err) {
+  fs.writeFile(`./json/${themeObj.name}.json`, jsonContent, 'utf8', function (err) {
     if (err) {
-      console.log(`An error occured while writing ${themeName} JSON Object to File.`)
+      console.log(`An error occured while writing ${themeObj.name} JSON Object to File.`)
       return console.log(err);
     }
 
-    console.log(`${themeName}.json has been saved in the json directory.`);
+    console.log(`${themeObj.name}.json has been saved in the json directory.`);
   });
-
 })
