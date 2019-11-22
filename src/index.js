ce709c604b2b79180ef856e4e5cd39b27cc2f65a
Cameron Wengert <cameron.wengert@mx.com>
2019-11-22 10:24:57 -0500

    Remove webpack and go to Babel only solution

--- src/index.js
+++ src/index.js
@@ -2,23 +2,15 @@ import core from 'src/tokens/core'
 import backgroundColors from 'src/tokens/backgroundColors'
 import spacing from 'src/tokens/spacing'
 
-const themeNames = ['light', 'dark']
-
-const exportObject = {}
-
-themeNames.forEach(themeName => {
-  const theme = {
-    ...core,
-    BackgroundColors: {
-      ...backgroundColors[themeName]
-    },
-    Spacing: {
-      ...core.Spacing,
-      ...spacing[themeName],
-    }
+const buildTheme = (themeName) => ({
+  ...core,
+  BackgroundColors: {
+    ...backgroundColors[themeName]
+  },
+  Spacing: {
+    ...core.Spacing,
+    ...spacing[themeName],
   }
-
-  exportObject[themeName] = theme
 })
 
-export default exportObject
+export default buildTheme
