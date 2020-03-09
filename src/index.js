efc32cc6d23bab094e7d78c86b0b6a82661a66ca
Jared Gerlach <jared.gerlach@mx.com>
2020-03-09 10:50:46 -0600

    add removePx param and helper util to remove px from core tokens for react native and other use cases

--- src/index.js
+++ src/index.js
@@ -7,7 +7,11 @@ import fontSize from 'src/tokens/fontSize'
 import spacing from 'src/tokens/spacing'
 import textColor from 'src/tokens/textColor'
 
-export const buildTheme = (themeName, customColors={}) => {
+export const buildTheme = (themeName, customColors = {}, removePx = false) => {
+  if (removePx) {
+    removePixels(core)
+  }
+
   const customCore = {
     ...core,
     Color: {
@@ -38,6 +42,23 @@ export const buildTheme = (themeName, customColors={}) => {
     },
     TextColor: textColor[themeName](customCore),
   }
+}
+
+const removePixels = core => {
+  Object.keys(core).forEach(key => {
+    const category = core[key]
+    if (
+      key === 'BorderRadius' ||
+      key === 'FontSize' ||
+      key === 'LineHeight' ||
+      key === 'Spacing' ||
+      key === 'MediaQuery'
+    ) {
+      Object.keys(category).forEach(property => {
+        category[property] = parseInt(category[property], 10)
+      })
+    }
+  })
 }
 
 export const light = buildTheme('light')
